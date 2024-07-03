75eacf5b1e9537daed837006aecca759c9ada335
bob <bobmcwishon@gmail.com>
2024-07-03 11:44:30 +0200

    added fetching data from api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import './style.css'
+import "./style.css";
 
-const apiKey = "68752630d7b8413394e160934242806"
-const currentUrl = "http://api.weatherapi.com/v1/current.json"
-const forecastUrl = "http://api.weatherapi.com/v1/forecast.json"
+const apiKey = "68752630d7b8413394e160934242806";
+const currentUrl = "http://api.weatherapi.com/v1/current.json";
+const forecastUrl = "http://api.weatherapi.com/v1/forecast.json";
 
-const locationform = document.querySelector('form')
-async function fetchWeather(form){
-    form.preventDefault();
-    alert(form.location)
+const locationform = document.querySelector("form");
+const forminput = document.querySelector("#location");
+async function fetchWeather(form) {
+  form.preventDefault();
+  await fetch(`${currentUrl}?key=${apiKey}&q=${forminput.value}`).then(
+    (response) => {console.log(response.json())}
+  );
 }
-locationform.addEventListener('submit',fetchWeather)
+locationform.addEventListener("submit", fetchWeather);
