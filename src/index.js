26b005b1bea2c78c9a5f1006623f664a7fae00c6
bob <bobmcwishon@gmail.com>
2024-07-04 18:17:03 +0200

    added styling to css

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ function drawCurrentWeather(data) {
   const location = document.createElement("div");
   location.classList.add("current");
   const country = document.createElement("h3");
-  console.log(data);
   country.innerText = data.location.country;
   const city = document.createElement("h1");
   city.innerText = data.location.name;
@@ -39,15 +38,9 @@ function drawCurrentWeather(data) {
   condition.innerText = data.current.condition.text;
   const temperature = document.createElement("h3");
   temperature.innerText = unit.checked ? `${data.current.temp_c} C` : `${data.current.temp_f} F`;
-  const locationBG = document.createElement('div')
   location.appendChild(condition);
   location.appendChild(temperature);
-  locationBG.style.backgroundColor = 'rgba(0,0,0,0.5)'
-  locationBG.style.width = '30%'
-  locationBG.style.height = '100%'
-  location.style.color = 'white';
-  locationBG.appendChild(location)
-  weatherBody.appendChild(locationBG);
+  weatherBody.appendChild(location);
   container.appendChild(weatherBody);
     const query = data.current.condition.text.replace(/\s/g, '-')
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=outside-${query}-weather&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips`, {mode: 'cors'})
