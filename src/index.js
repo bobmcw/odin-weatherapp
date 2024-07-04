f9357d5b20a34692fddf436ed72afec44f0bac4c
bob <bobmcwishon@gmail.com>
2024-07-04 13:14:01 +0200

    added drawing the location

--- src/index.js
+++ src/index.js
@@ -8,10 +8,28 @@ const locationform = document.querySelector("form");
 const forminput = document.querySelector("#location");
 async function fetchCurrentWeather(form) {
   form.preventDefault();
-  await fetch(`${currentUrl}?key=${apiKey}&q=${forminput.value}`).then(
-    (response) => {console.log(response.json())}
-  );
+  await fetch(`${currentUrl}?key=${apiKey}&q=${forminput.value}`, {
+    mode: "cors",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      drawCurrentWeather(data);
+    });
+}
+function drawCurrentWeather(data) {
+  const container = document.querySelector(".container");
+  console.log(data);
+  const currentBody = document.createElement("div");
+  currentBody.classList.add("current");
+  const country = document.createElement("h3");
+  console.log(data);
+  country.innerText = data.location.country;
+  const city = document.createElement('h1');
+  city.innerText = data.location.name;
+  currentBody.appendChild(city);
+  currentBody.appendChild(country);
+  container.appendChild(currentBody)
 }
 locationform.addEventListener("submit", fetchCurrentWeather);
-
-
